350f489045b9e1b7b74ea9c5ac67fccbedb8b023
chirp <ch1rp@protonmail.com>
2023-06-02 21:09:01 +0100

    Implement Equip and Race enums

---
+++ Xande/Enums/Equip.cs
@@ -0,0 +1,32 @@
+﻿namespace Xande.Enums;
+
+// https://github.com/ktisis-tools/Ktisis/blob/a9345539b8957d4af34715ca6907fbd7df196738/Ktisis/Structs/Actor/Equipment.cs#L35
+
+public enum EquipSlot : uint {
+    Head,
+    Chest,
+    Hands,
+    Legs,
+    Feet,
+    Earring,
+    Necklace,
+    Bracelet,
+    RingRight,
+    RingLeft
+}
+
+public enum ModelSlot : uint {
+    Hat,
+    Top,
+    Gloves,
+    Legs,
+    Shoes,
+    Ears,
+    Neck,
+    Wrists,
+    RingRight,
+    RingLeft,
+    Hair,
+    Face,
+    TailEars
+}
