116ba90252e7dc1f698f34c6a063f735098891b1
adamm789 <musician903@gmail.com>
2023-06-07 18:13:09 -0500

    Update VertexDataBuilder.cs
    add ability to write bitangents

--- Xande/Models/Import/VertexDataBuilder.cs
+++ Xande/Models/Import/VertexDataBuilder.cs
@@ -12,17 +12,22 @@ using System.Threading.Tasks;
 namespace Xande.Models.Import {
     internal class VertexDataBuilder {
 
-        private static List<byte> GetVertexData( MeshPrimitive primitive, int index, MdlStructs.VertexElement ve, Dictionary<int, int>? blendIndicesDict = null, IReadOnlyDictionary<string, Accessor>? shapeAccessor = null ) {
-            var vector4 = GetVector4( primitive, index, ( Vertex.VertexUsage )ve.Usage, blendIndicesDict, shapeAccessor );
+        private static List<byte> GetVertexData( MeshPrimitive primitive, int index, MdlStructs.VertexElement ve, Dictionary<int, int>? blendIndicesDict = null, IReadOnlyDictionary<string, Accessor>? shapeAccessor = null, List<Vector4>? bitangents = null ) {
+            var vector4 = GetVector4( primitive, index, ( Vertex.VertexUsage )ve.Usage, blendIndicesDict, shapeAccessor, bitangents );
             return GetBytes( vector4, ( Vertex.VertexType )ve.Type );
         }
         private static Vector4 GetVector4( MeshPrimitive primitive, int index, Vertex.VertexUsage usage,
-            Dictionary<int, int>? blendIndexDict = null, IReadOnlyDictionary<string, Accessor>? shapeAccessor = null) {
+            Dictionary<int, int>? blendIndexDict = null, IReadOnlyDictionary<string, Accessor>? shapeAccessor = null,
+            List<Vector4>? bitangents = null ) {
             var vector4 = new Vector4( 0, 0, 0, 0 );
+
             switch( usage ) {
                 case Vertex.VertexUsage.Position:
                     Accessor? positionAccessor = null;
                     shapeAccessor?.TryGetValue( "POSITION", out positionAccessor );
+                    if( positionAccessor != null ) {
+                        PluginLog.Debug( "shape is active" );
+                    }
                     var positions = positionAccessor?.AsVector3Array() ?? primitive.GetVertexAccessor( "POSITION" )?.AsVector3Array();
                     if( positions != null && positions.Count > index ) {
                         vector4 = new Vector4( positions[index], 1 );
@@ -36,8 +41,6 @@ namespace Xande.Models.Import {
                     break;
                 case Vertex.VertexUsage.BlendIndices:
                     var blendIndices = primitive.GetVertexAccessor( "JOINTS_0" )?.AsVector4Array();
-                    // Seems like SE may swap around BlendIndices if an BlendIndex zero appears not in the 0-th index and has more than one weight
-                    // e.g. would-be: 1, 0 (has weight), _, _ => 0, 1, _, _
                     if( blendIndices != null && blendIndices.Count > index ) {
                         vector4 = blendIndices[index];
                         if( blendIndexDict != null ) {
@@ -60,7 +63,7 @@ namespace Xande.Models.Import {
                 case Vertex.VertexUsage.UV:
                     var texCoords = primitive.GetVertexAccessor( "TEXCOORD_0" )?.AsVector2Array();
                     if( texCoords?.Count > index ) {
-                        vector4 = new( texCoords[index].X, texCoords[index].Y, 0, 0);
+                        vector4 = new( texCoords[index].X, texCoords[index].Y - 1, -1, -2 );
                     }
                     break;
                 case Vertex.VertexUsage.Tangent2:
@@ -68,15 +71,29 @@ namespace Xande.Models.Import {
                     vector4 = new( 0, 0, 0, 0 );
                     break;
                 case Vertex.VertexUsage.Tangent1:
-                    var tangents = primitive.GetVertexAccessor( "TANGENT" )?.AsVector4Array();
-                    if( tangents?.Count > index ) {
-                        vector4 = tangents[index];
+                    if( bitangents != null ) {
+                        var vector3 = new Vector3( bitangents[index].X, bitangents[index].Y, bitangents[index].Z );
+                        vector3 = Vector3.Normalize( vector3 );
+                        vector3 += Vector3.One;
+                        vector3 /= 2.0f;
+                        vector3 -= Vector3.One;
+                        var handedness = bitangents[index].W > 0 ? 0 : 1;
+                        vector4 = new Vector4( vector3, handedness );
+                    }
+                    else {
+                        var tangents = primitive.GetVertexAccessor( "TANGENT" )?.AsVector4Array();
+                        if( tangents?.Count > index ) {
+                            vector4 = tangents[index];
+                        }
                     }
                     break;
                 case Vertex.VertexUsage.Color:
                     var colors = primitive.GetVertexAccessor( "COLOR_0" )?.AsVector4Array();
                     if( colors?.Count > index ) {
                         vector4 = colors[index];
+                    }
+                    else {
+                        vector4 = new Vector4( 1, 1, 1, 1 );
                     }
                     break;
             }
@@ -104,10 +121,10 @@ namespace Xande.Models.Import {
                     ret.Add( ( byte )vector4.W );
                     break;
                 case Vertex.VertexType.ByteFloat4:
-                    ret.Add( ( byte )( vector4.X * 255f ) );
-                    ret.Add( ( byte )( vector4.Y * 255f ) );
-                    ret.Add( ( byte )( vector4.Z * 255f ) );
-                    ret.Add( ( byte )( vector4.W * 255f ) );
+                    ret.Add( ( byte )Math.Round( vector4.X * 255f ) );
+                    ret.Add( ( byte )Math.Round( vector4.Y * 255f ) );
+                    ret.Add( ( byte )Math.Round( vector4.Z * 255f ) );
+                    ret.Add( ( byte )Math.Round( vector4.W * 255f ) );
                     break;
                 case Vertex.VertexType.Half2:
                     ret.AddRange( BitConverter.GetBytes( ( Half )vector4.X ) );
@@ -124,7 +141,7 @@ namespace Xande.Models.Import {
 
         }
 
-        public static Dictionary<int, List<byte>> GetVertexData( SubmeshBuilder submesh, MdlStructs.VertexDeclarationStruct vertexDeclarations, List<string>? strings = null, Dictionary<int, int>? blendIndicesDict = null ) {
+        public static Dictionary<int, List<byte>> GetVertexData( SubmeshBuilder submesh, MdlStructs.VertexDeclarationStruct vertexDeclarations, List<string>? strings = null, Dictionary<int, int>? blendIndicesDict = null, List<Vector4>? bitangents = null ) {
             var streams = new Dictionary<int, List<byte>>();
             var ret = new List<byte>();
 
@@ -136,11 +153,12 @@ namespace Xande.Models.Import {
                         for( var declarationId = 0; declarationId < vertexDeclarations.VertexElements.Length; declarationId++ ) {
                             var ve = vertexDeclarations.VertexElements[declarationId];
                             if( ve.Stream == 255 ) { break; }
-                            if (!streams.ContainsKey(ve.Stream)) {
+                            if( !streams.ContainsKey( ve.Stream ) ) {
                                 streams.Add( ve.Stream, new List<byte>() );
                             }
                             var currStream = streams[ve.Stream];
-                            currStream.AddRange( GetVertexData( primitive, vertexId, ve, blendIndicesDict ) );
+
+                            streams[ve.Stream].AddRange( GetVertexData( primitive, vertexId, ve, blendIndicesDict, null, bitangents ) );
                         }
                     }
                 }
