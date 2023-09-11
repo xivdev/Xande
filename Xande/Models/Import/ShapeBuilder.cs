9510004f7fcdbab71777d9fdd27c80c232be3b03
adamm789 <musician903@gmail.com>
2023-09-11 11:29:12 -0500

    minor formatting changes

--- Xande/Models/Import/ShapeBuilder.cs
+++ Xande/Models/Import/ShapeBuilder.cs
@@ -43,7 +43,6 @@ namespace Xande.Models.Import {
                     ReplacingVertexIndex = ( ushort )DifferentVertices.IndexOf( ( int )vertexIdx )
                 } );
             }
-
         }
     }
 }
